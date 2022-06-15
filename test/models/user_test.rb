b6a9d9d3d84fd6b2724d072fd57f205a7b4306ff
Gitbuster1 <tw_tibi@yahoo.ro>
2022-06-15 05:18:33 +0300

    Finish user signup

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -61,7 +61,7 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-  test "password should be present (nonblank)" do
+  test "password should be present (non blank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert_not @user.valid?
   end
